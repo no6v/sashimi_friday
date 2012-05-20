eef510cd20a70c2789a6fe83515b71343276c15c
Nobuhiro IMAI <nov@yo.rim.or.jp>
2012-05-20 16:28:08 +0900

    method entries

--- sashimi.rb
+++ sashimi.rb
@@ -1,2 +1,145 @@
 module Sashimi
+  def all?
+  end
+
+  def any?
+  end
+
+  def chunk
+  end
+
+  def collect
+  end
+
+  def collect_concat
+  end
+
+  def count
+  end
+
+  def cycle
+  end
+
+  def detect
+  end
+
+  def drop
+  end
+
+  def drop_while
+  end
+
+  def each_cons
+  end
+
+  def each_entry
+  end
+
+  def each_slice
+  end
+
+  def each_with_index
+  end
+
+  def each_with_object
+  end
+
+  def entries
+  end
+
+  def find
+  end
+
+  def find_all
+  end
+
+  def find_index
+  end
+
+  def first
+  end
+
+  def flat_map
+  end
+
+  def grep
+  end
+
+  def group_by
+  end
+
+  def include?
+  end
+
+  def inject
+  end
+
+  def lazy
+  end
+
+  def map
+  end
+
+  def max
+  end
+
+  def max_by
+  end
+
+  def member?
+  end
+
+  def min
+  end
+
+  def min_by
+  end
+
+  def minmax
+  end
+
+  def minmax_by
+  end
+
+  def none?
+  end
+
+  def one?
+  end
+
+  def partition
+  end
+
+  def reduce
+  end
+
+  def reject
+  end
+
+  def reverse_each
+  end
+
+  def select
+  end
+
+  def slice_before
+  end
+
+  def sort
+  end
+
+  def sort_by
+  end
+
+  def take
+  end
+
+  def take_while
+  end
+
+  def to_a
+  end
+
+  def zip
+  end
 end
